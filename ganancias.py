b5eabc1db28d53a33d6fe7b988a44c3357ecb134
MathiuBYJUS <95060413+MathiuBYJUS@users.noreply.github.com>
2023-03-06 20:00:22 -0600

    Add files via upload

---
+++ ganancias.py
@@ -0,0 +1,6 @@
+meses=["enero","febrero","marzo","abirl","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"]
+ganancias=[10000,20000,30000,40000,50000,60000,70000,80000,90000,100000]
+ganancia_maxima = max(ganancias)
+print(ganancia_maxima)
+mes_de_ganancia = max(meses)
+print(mes_de_ganancia)
